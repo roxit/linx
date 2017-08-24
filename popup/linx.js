c4ed74699ebfa2251feab65b7d63ce0228f68927
Shanshi Shi <shishanshi@gmail.com>
2017-08-25 00:16:49 +0800

    copy the real link

--- popup/linx.js
+++ popup/linx.js
@@ -7,8 +7,9 @@ function getCurrentTab(cb) {
     }
   });
 }
-// https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
+
 function escapeHTML(str) {
+    // https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
     // Note: string cast using String; may throw if `str` is non-serializable, e.g. a Symbol.
     // Most often this is not the case though.
     return String(str)
@@ -29,14 +30,15 @@ function transTab(tab) {
 function processLink(tab) {
   var res = transTab(tab);
   var url = res[0];
-  var safeUrl = escapeHTML(url);
   var text = res[1];
-  var safeText = escapeHTML(text);
-  link = `<a href="${safeUrl}">${safeText}</a>`;
   document.querySelector("#url").value = url;
   document.querySelector("#text").value = text;
+  var safeUrl = escapeHTML(url);
+  var safeText = escapeHTML(text);
+  link = `<a href="${safeUrl}">${safeText}</a>`;
   var el = document.querySelector('#link');
-  el.innerHTML = link;
+  el.href = safeUrl;
+  el.text = safeText;
 }
 
 function setLink() {
@@ -59,5 +61,17 @@ function copyText() {
   copy("#text");
 }
 
+function copyLink() {
+  var el = document.querySelector("#link");
+  // https://stackoverflow.com/questions/6139107/programmatically-select-text-in-a-contenteditable-html-element
+  var range = document.createRange();
+  range.selectNodeContents(el);
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+  document.execCommand('copy');
+}
+
 document.querySelector("#url").addEventListener("click", copyURL);
 document.querySelector("#text").addEventListener("click", copyText);
+document.querySelector("#link").addEventListener("click", copyLink);
